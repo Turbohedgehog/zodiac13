54e8f241f05ee18a2f7973dd917a48ac934d38c9
turbohedgehog <unlinker@mail.ru>
2025-05-25 00:44:10 +0300

    VCPKG manifest mode

--- src/zodiac13/main.cpp
+++ src/zodiac13/main.cpp
@@ -2,8 +2,12 @@
 
 #include <z13_launcher/z13_launcher.h>
 
+#include "flex_test.h"
+
 int main(int argc, char *argv[]) {
   z13::Zodiac13Launcher zodiac13_launcher;
+
+  test_flex2(argc, argv);
 
   return zodiac13_launcher.Run(argc, argv);
 }
